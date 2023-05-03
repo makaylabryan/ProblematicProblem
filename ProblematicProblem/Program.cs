e2e6a0ddbb00f6e43c5b1569b1303210f4afa129
Makayla Bryan <makaylabryan@gmail.com>
2023-05-03 13:00:45 -0400

    Whoops

--- ProblematicProblem/Program.cs
+++ ProblematicProblem/Program.cs
@@ -119,7 +119,7 @@ namespace ProblematicProblem
 
                     }
 
-                    Console.WriteLine($"Ah got it! {randomActivity}, your random activity is: {userName}! Is this ok or do you want to grab another activity? Keep/Redo: ");
+                    Console.WriteLine($"Ah got it! {userName}, your random activity is: {randomActivity}! Is this ok or do you want to grab another activity? Keep/Redo: ");
                     Console.WriteLine();
                     cont = ( Console.ReadLine().ToLower() == "redo") ? true : false;
                 }
